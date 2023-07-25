728b21d59b8b3a11bf81d08301e710ed77de2f19
Gustavo Lima <gustavo.f12@hotmail.com>
2023-07-25 17:40:15 -0300

    Logic has been implemented and was added new movie items to simulate it working.

--- app/src/main/java/com/gustavolima/myrecycleviewdemo/MainActivity.kt
+++ app/src/main/java/com/gustavolima/myrecycleviewdemo/MainActivity.kt
@@ -2,10 +2,86 @@ package com.gustavolima.myrecycleviewdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.gustavolima.myrecycleviewdemo.adapter.AdapterMovies
+import com.gustavolima.myrecycleviewdemo.databinding.ActivityMainBinding
+import com.gustavolima.myrecycleviewdemo.model.Movie
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapterMovies: AdapterMovies
+    private val moviesList: MutableList<Movie> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val recyclerViewMovies = binding.recyclerViewMovies
+        recyclerViewMovies.layoutManager = LinearLayoutManager(this)
+        recyclerViewMovies.setHasFixedSize(true)
+        adapterMovies = AdapterMovies(this, moviesList)
+        recyclerViewMovies.adapter = adapterMovies
+        movies()
+    }
+    private fun movies(){
+        val movie1 = Movie("Mad Max: Fury Road")
+        moviesList.add(movie1)
+
+        val movie2 = Movie("John Wick")
+        moviesList.add(movie2)
+
+        val movie3 = Movie("Die Hard")
+        moviesList.add(movie3)
+
+        val movie4 = Movie("The Dark Knight")
+        moviesList.add(movie4)
+
+        val movie5 = Movie("Gladiator")
+        moviesList.add(movie5)
+
+        val movie6 = Movie("Inception")
+        moviesList.add(movie6)
+
+        val movie7 = Movie("The Avengers")
+        moviesList.add(movie7)
+
+        val movie8 = Movie("The Avengers")
+        moviesList.add(movie8)
+
+        val movie9 = Movie("Mission: Impossible")
+        moviesList.add(movie9)
+
+        val movie10 = Movie("The Matrix")
+        moviesList.add(movie10)
+
+        val movie11 = Movie("Mad Max: Fury Road")
+        moviesList.add(movie11)
+
+        val movie12 = Movie("Black Panther")
+        moviesList.add(movie12)
+
+        val movie13 = Movie("Jurassic Park")
+        moviesList.add(movie13)
+
+        val movie14 = Movie("Gladiator")
+        moviesList.add(movie14)
+
+        val movie15 = Movie("Avatar")
+        moviesList.add(movie15)
+
+        val movie16 = Movie("Fast & Furious")
+        moviesList.add(movie16)
+
+        val movie17 = Movie("Wonder Woman")
+        moviesList.add(movie17)
+
+        val movie18 = Movie("The Lord of the Rings: The Fellowship of the Ring")
+        moviesList.add(movie18)
+
+        val movie19 = Movie("The Terminator")
+        moviesList.add(movie19)
+
+        val movie20 = Movie("Black Widow")
+        moviesList.add(movie20)
     }
 }
--- app/src/main/java/com/gustavolima/myrecycleviewdemo/model/Movie.kt
+++ app/src/main/java/com/gustavolima/myrecycleviewdemo/model/Movie.kt
@@ -1,6 +1,3 @@
 package com.gustavolima.myrecycleviewdemo.model
 
-class Movie {
-    val title:String? = null
-
-}
+data class Movie(val title:String? = null)
